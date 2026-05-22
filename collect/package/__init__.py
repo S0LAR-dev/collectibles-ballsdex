48597b34738b6c25f7eea235830879c4c2d467b3
CrashTestAlex <alexkola109@icloud.com>
2026-05-22 19:56:25 -0400

    Refactor setup function to add AdminCollectiblesCog

--- collect/package/__init__.py
+++ collect/package/__init__.py
@@ -9,9 +9,4 @@ if TYPE_CHECKING:
 
 async def setup(bot: "BallsDexBot"):
     await bot.add_cog(CollectiblesCog(bot))
-
-    admin_cog = AdminCollectiblesCog(bot)
-    await bot.add_cog(admin_cog)
-    root_admin = bot.cogs.get("Admin")
-    if root_admin is not None and hasattr(root_admin, "admin"):
-        root_admin.admin.add_command(admin_cog.collectibles)
+    await bot.add_cog(AdminCollectiblesCog(bot))
