40c37f55cbb5d764abaf11c984abbb7a5c11c52d
S0LAR-dev <rishin.sethi@gmail.com>
2026-06-23 21:32:58 -0400

    Update cog.py

--- collect/package/cog.py
+++ collect/package/cog.py
@@ -171,6 +171,7 @@ class BuyButton(discord.ui.Button):
     def __init__(self, currency_symbol: str):
         super().__init__(
             style=discord.ButtonStyle.success,
+            partial_emoji=discord.PartialEmoji(name="Rosaries", id=1513372255999098910)
             label=f"<:Rosaries:1513372255999098910> Buy",
             custom_id="buy"
         )
